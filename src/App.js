3a578476600626c374fcf18b6be5a0309d9f7bcf
nikhil <nikhiltomy@au-ki.com>
2021-03-04 07:41:05 +0530

    layout added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import {CardList} from './component/card-list/card-list.component';
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      monsters:[]
+    }
+  }
+  
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=>this.setState({monsters:users}))
+  }
+  render() {
+    return (
+      <div className="App">
+          <input type="search"/>
+          <CardList monsters={this.state.monsters}/>
+      </div>
+    )
+  }
 }
 
-export default App;
